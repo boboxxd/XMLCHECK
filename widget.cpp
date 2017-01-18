29278444940537b58d2140e3949d50922db4b4f7
boboxxd <1539731480@qq.com>
2017-01-18 17:57:10 +0800

    实现外部框架，打开文件和打开文件夹，上一页和下一页

--- widget.cpp
+++ widget.cpp
@@ -6,9 +6,84 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    connect(ui->openbtn,SIGNAL(clicked(bool)),this,SLOT(onopenBtn()));
+    connect(ui->openpathbtn,SIGNAL(clicked(bool)),this,SLOT(onopenpathBtn()));
+    connect(ui->beforebtn,SIGNAL(clicked(bool)),this,SLOT(onbeforeBtn()));
+    connect(ui->nextbtn,SIGNAL(clicked(bool)),this,SLOT(onnextBtn()));
+    connect(ui->sendbtn,SIGNAL(clicked(bool)),this,SLOT(onsendBtn()));
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+int Widget::onopenBtn()
+{
+    QString filename= QFileDialog::getOpenFileName(this,tr("Open File"),\
+                                                    NULL,\
+                                                    "Images (*.jpeg *.jpg)",\
+                                                    0);
+    QFileInfo fileInfo(filename);
+    QDir dir(fileInfo.absolutePath());
+    QStringList filters;
+    filters << "*.jpg" << "*.jpeg" ;
+    dir.setNameFilters(filters);
+    dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+    dir.setSorting(QDir::Name);
+    list = dir.entryInfoList();
+    index=list.indexOf(fileInfo);//获取文件在目录下的序号
+    fileInfo = list.at(index);
+
+    qDebug()<<index<<fileInfo.absoluteFilePath();
+
+    return 0;
+}
+
+int Widget::onopenpathBtn()
+{
+    index=0;
+    QString temp=QFileDialog::getExistingDirectory ();
+    QDir dir(temp);
+    QStringList filters;
+    filters << "*.jpg" << "*.jpeg" ;
+    dir.setNameFilters(filters);
+    dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+    dir.setSorting(QDir::Name);
+    list = dir.entryInfoList();
+    QFileInfo fileInfo;
+    fileInfo = list.at(index);
+    qDebug()<<index<<fileInfo.absoluteFilePath();
+    return 0;
+}
+
+int Widget::onbeforeBtn()
+{
+    index--;
+   if(index<0)
+   {
+    index++;
+   }
+    QFileInfo fileInfo = list.at(index);
+    qDebug()<<index<<fileInfo.absoluteFilePath();
+    return 0;
+    return 0;
+}
+
+int Widget::onnextBtn()
+{
+    index++;
+   if(index>=list.count())
+   {
+    index--;
+   }
+    QFileInfo fileInfo = list.at(index);
+    qDebug()<<index<<fileInfo.absoluteFilePath();
+    return 0;
+}
+
+int Widget::onsendBtn()
+{
+
+    return 0;
 }
--- widget.h
+++ widget.h
@@ -2,7 +2,9 @@
 #define WIDGET_H
 
 #include <QWidget>
-
+#include<QDir>
+#include<QFileDialog>
+#include<qDebug>
 namespace Ui {
 class Widget;
 }
@@ -13,10 +15,23 @@ class Widget : public QWidget
 
 public:
     explicit Widget(QWidget *parent = 0);
+
     ~Widget();
+public:
+
+    QFileInfoList list; //保存的是路径下的文件列表；
+    int index;          //保存的是文件在列表中的序号；
+
+public slots:
+    int onopenBtn();
+    int onopenpathBtn();
+    int onbeforeBtn();
+    int onnextBtn();
+    int onsendBtn();
 
 private:
     Ui::Widget *ui;
+
 };
 
 #endif // WIDGET_H
